79594f84c991464b6c85d475814742530d8438c6
wjfc <yyhxjww@163.com>
2018-03-19 15:05:49 +0800

    dev分支添加

--- src/components/Person/Person.vue
+++ src/components/Person/Person.vue
@@ -26,7 +26,8 @@
             type: "video/mp4",
             src: "../../../src/assets/mp4/1.mp4"
           }],
-          add:'添加内容'
+          add:'添加内容',
+          add2: 'branch分支 新增内容'
         }
       }
     },
